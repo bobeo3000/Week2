d985468b8883a17386d79b33d2182fc77c0bb1b8
Marcy <marcienguy79@gmail.com>
2020-05-27 12:06:42 -0400

    Finished constructors

--- week2.cpp
+++ week2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Rectangle {
 private:
     int top, left, bottom, right;
-
+public:
     Rectangle(int t, int b, int l, int r);
     Rectangle(int width, int height) : Rectangle(0, height, 0, width) {};
     Rectangle();
