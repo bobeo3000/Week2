ca8e8fab245d901643ab0795b4105e8bb2444a2b
Marcy <marcienguy79@gmail.com>
2020-05-27 12:23:46 -0400

    Demo #3

--- week2.cpp
+++ week2.cpp
@@ -6,16 +6,16 @@ private:
     int top, left, bottom, right;
 public:
     Rectangle(int t, int b, int l, int r);
-    Rectangle(int width, int height) : Rectangle(0, height, 0, width) {};
+    Rectangle(int width, int height) : Rectangle(0, height, 0, width) {
+        cout << "In constructor with 2 parameters, width = " << width << " height = " << height << endl;
+    };
     Rectangle();
     ~Rectangle();
 };
 Rectangle::Rectangle(int t, int b, int l, int r) {
     cout << "In constructor with 4 parameters" << endl;
 }
-Rectangle::Rectangle(int width, int height) {
-    cout << "In constructor with 2 parameters, width = " << width << " height = " << height << endl;
-}
+
 Rectangle::Rectangle() {
     cout << "In constructor with 0 parameter" << endl;
 }
