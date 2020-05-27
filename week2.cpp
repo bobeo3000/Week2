67615f86b56d9553b6ec404311fdc5309193a78a
Marcy <marcienguy79@gmail.com>
2020-05-27 12:02:43 -0400

    Finished constructors

--- week2.cpp
+++ week2.cpp
@@ -1,8 +1,19 @@
 
 #include<iostream>
-
+using namespace std;
+class MyFirstClass {
+    MyFirstClass() {}
+};
+class MySecondClass {};
 int main(int argc, char **argv)
 {
-   std::cout << "Hello World!" << std::endl;
-   return 0;
+    class Rectangle {
+    private: 
+        int top, left, bottom, right;
+    
+        Rectangle(int t, int b, int l, int r);
+        Rectangle(int width, int height) : Rectangle(0, height, 0, width) {};
+        Rectangle();
+    };
+    
 }
