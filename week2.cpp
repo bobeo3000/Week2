f6c869bbf40d0444f368e8480de7fdfdfd91ba27
Marcy <marcienguy79@gmail.com>
2020-05-27 12:13:43 -0400

    Finished destructor

--- week2.cpp
+++ week2.cpp
@@ -8,9 +8,20 @@ public:
     Rectangle(int t, int b, int l, int r);
     Rectangle(int width, int height) : Rectangle(0, height, 0, width) {};
     Rectangle();
+    ~Rectangle();
 };
-
-
+Rectangle::Rectangle(int t, int b, int l, int r) {
+    cout << "In constructor with 4 parameters" << endl;
+}
+Rectangle::Rectangle(int width, int height) {
+    cout << "In constructor with 2 parameters, width = " << width << " height = " << height << endl;
+}
+Rectangle::Rectangle() {
+    cout << "In constructor with 0 parameter" << endl;
+}
+Rectangle:: ~Rectangle() {
+    cout << "In destructor" << endl;
+}
 int main(int argc, char **argv)
 {
    
